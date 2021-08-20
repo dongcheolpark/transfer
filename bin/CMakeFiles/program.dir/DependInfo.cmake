24945d2512c4c703fc758f292707602b72a85dc2
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-08-20 22:16:41 +0900

    a

---
+++ bin/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/parkdongcheol/programming/transfer/src/ImageLoader/Image.cpp" "CMakeFiles/program.dir/src/ImageLoader/Image.cpp.o" "gcc" "CMakeFiles/program.dir/src/ImageLoader/Image.cpp.o.d"
+  "/Users/parkdongcheol/programming/transfer/src/ImageLoader/ImageTransfer.cpp" "CMakeFiles/program.dir/src/ImageLoader/ImageTransfer.cpp.o" "gcc" "CMakeFiles/program.dir/src/ImageLoader/ImageTransfer.cpp.o.d"
+  "/Users/parkdongcheol/programming/transfer/src/ImageLoader/PngImage.cpp" "CMakeFiles/program.dir/src/ImageLoader/PngImage.cpp.o" "gcc" "CMakeFiles/program.dir/src/ImageLoader/PngImage.cpp.o.d"
+  "/Users/parkdongcheol/programming/transfer/src/main.cpp" "CMakeFiles/program.dir/src/main.cpp.o" "gcc" "CMakeFiles/program.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
