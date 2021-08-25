0497f882ee05b235e5610ef184e5008b3666dee8
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-08-25 17:49:08 +0900

    add FreeImage

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <fstream>
+#include "FreeImage.h"
 #include "ImageTransfer.hpp"
 
 using namespace std;
 
 int main() {
-	ImageTransfer test("../test2.png");
-	test.run();
+	const char * imagePath = "../test2.png";
+	FreeImage_Initialise(TRUE);
+
+	FIBITMAP *dib = FreeImage_Load(FIF_PNG,imagePath,PNG_DEFAULT);
+	int width = FreeImage_GetWidth(dib);
+	int height = FreeImage_GetHeight(dib);
+	cout<<width<<height;
 }
